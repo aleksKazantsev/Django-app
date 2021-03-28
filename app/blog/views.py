3c28794ba28553ee58da2249f9554a9cd464c1ff
Aleks Kazantsev <9224762646@mail.ru>
2021-03-29 03:18:30 +0500

    add bootstrap, add pagination

--- app/blog/views.py
+++ app/blog/views.py
@@ -9,13 +9,8 @@ from .models import *
 from .utils import *
 
 
-#menu = [{'title': "О сайте", 'url_name': 'about'},
-#        {'title': "Добавить статью", 'url_name': 'add_post'},
-#        {'title': "Обратная связь", 'url_name': 'contact'},
-#        {'title': "Войти", 'url_name': 'login'}
-#]
-
 class BlogHome(DataMixin, ListView):
+    #paginate_by = 2
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
@@ -23,10 +18,6 @@ class BlogHome(DataMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Главная страница', cat_selected=0)
-        #context['menu'] = menu
-        #context['title'] = 'Главная страница'
-        #context['cat_selected'] = 
-        #context = dict(list(context.items())+list(c_def.items()))
         return dict(list(context.items())+list(c_def.items()))
     
     def get_queryset(self):
@@ -37,15 +28,11 @@ class AddPost(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'blog/addpost.html'
     success_url = reverse_lazy('home')
-    #login_url = '/admin/'
     raise_exception = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Добавление поста')
-        #context['menu'] = menu
-        #context['title'] = 'Добавление поста'
-        #return context
+        c_def = self.get_user_context(title='Добавление поста', cat_selected='')
         return dict(list(context.items())+list(c_def.items()))
 
 
@@ -57,14 +44,12 @@ class ShowPost(DataMixin, DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title=context['post'])
-        #context['menu'] = menu
-        #context['title'] = context['post']
-        #return context
+        c_def = self.get_user_context(title=context['post'], cat_selected='')
         return dict(list(context.items())+list(c_def.items()))
 
 
 class PostCategory(DataMixin, ListView):
+    #paginate_by = 2
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
@@ -73,10 +58,6 @@ class PostCategory(DataMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat), cat_selected=self.kwargs['cat_slug'])
-        #context['menu'] = menu
-        #context['title'] = 'Категория - ' + str(context['posts'][0].cat)
-        #context['cat_selected'] = self.kwargs['cat_slug']
-        #return context
         return dict(list(context.items())+list(c_def.items()))
     
     def get_queryset(self):
